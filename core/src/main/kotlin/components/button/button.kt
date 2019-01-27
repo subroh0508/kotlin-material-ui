b3be84e8198912cc14ff0c3fceb204a6c4cc890f
subroh0508 <subroh.0508@gmail.com>
2019-01-28 01:37:03 +0900

    [edit]Insert @suppress("UnsafeCastFromDynamic")

--- core/src/main/kotlin/components/button/button.kt
+++ core/src/main/kotlin/components/button/button.kt
@@ -6,6 +6,8 @@ import react.RState
 
 @JsModule("@material-ui/core/Button")
 private external val buttonModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
 private val buttonComponent: RComponent<ButtonProps, RState> = buttonModule.default
 
 fun RBuilder.button(caption: String = "", block: ButtonElementBuilder.() -> Unit)
--- core/src/main/kotlin/components/icon/icon.kt
+++ core/src/main/kotlin/components/icon/icon.kt
@@ -6,6 +6,8 @@ import react.RState
 
 @JsModule("@material-ui/core/Icon")
 private external val iconModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
 private val iconComponent: RComponent<IconProps, RState> = iconModule.default
 
 fun RBuilder.icon(iconName: String, block: IconElementBuilder.() -> Unit)
