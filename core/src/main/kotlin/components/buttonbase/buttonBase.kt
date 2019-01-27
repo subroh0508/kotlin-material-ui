03c2d4b79b21d530b494f5aa288db15d62bc860e
subroh0508 <subroh.0508@gmail.com>
2019-01-27 23:08:36 +0900

    [add]Add ButtonBaseBuilder and required classes

---
+++ core/src/main/kotlin/components/buttonbase/buttonBase.kt
@@ -0,0 +1,13 @@
+package components.buttonbase
+
+import react.RBuilder
+import react.RComponent
+import react.RState
+
+
+@JsModule("@material-ui/core/Button")
+private external val buttonModule: dynamic
+private val buttonComponent: RComponent<ButtonBaseProps, RState> = buttonModule.default
+
+fun RBuilder.buttonBase(block: ButtanBaseElementBuilder.() -> Unit)
+    = child(ButtanBaseElementBuilder(buttonComponent).apply(block).create())
