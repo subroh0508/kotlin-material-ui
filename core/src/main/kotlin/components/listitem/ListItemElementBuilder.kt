4d3ad2d8d3a02c529ee947379bc96c3db872bbc7
subroh0508 <subroh.0508@gmail.com>
2019-01-29 21:43:31 +0900

    [edit]Fix ListItemElementBuilder

--- core/src/main/kotlin/components/listitem/ListItemElementBuilder.kt
+++ core/src/main/kotlin/components/listitem/ListItemElementBuilder.kt
@@ -1,60 +1,50 @@
 package components.listitem
 
 import components.MaterialElementBuilder
+import components.consumers
 import components.listitem.enums.ListItemAlignItem
 import components.listitemsecondaryaction.ListItemSecondaryActionProps
-import kotlinext.js.jsObject
+import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
 import react.RComponent
+import react.RProps
 import react.RState
+import kotlin.reflect.KClass
 
-class ListItemElementBuilder internal constructor(
-    override var type: RComponent<ListItemProps, RState>,
-    override var attrs: ListItemProps = jsObject {  }
-) : MaterialElementBuilder<ListItemProps>(attrs),
-    ListItemAttributes by AttributesImpl(attrs) {
+class ListItemElementBuilder<T: Tag> internal constructor(
+    type: RComponent<RProps, RState>,
+    tag: KClass<T>,
+    factory: (TagConsumer<Unit>) -> T = consumers(tag)
+) : MaterialElementBuilder<Tag>(type, factory) {
 
-    internal class AttributesImpl(private val props: ListItemProps) : ListItemAttributes {
-        override var className: String?
-            get() = props.className
-            set(value) { props.className = value }
-        override var alignItems: String
-            get() = props.alignItems
-            set(value) { props.alignItems = value }
-        override var button: Boolean
-            get() = props.button
-            set(value) { props.button = value }
-        override var classes: Any
-            get() = props.classes
-            set(value) { props.classes = value }
-        override var component: String
-            get() = props.component
-            set(value) { props.component = value }
-        override var containerComponent: String
-            get() = props.containerComponent
-            set(value) { props.containerComponent = value }
-        override var containerProps: ListItemSecondaryActionProps
-            get() = props.containerProps
-            set(value) { props.containerProps = value }
-        override var dense: Boolean
-            get() = props.dense
-            set(value) { props.dense = value }
-        override var disabled: Boolean
-            get() = props.disabled
-            set(value) { props.disabled = value }
-        override var disableGutters: Boolean
-            get() = props.disableGutters
-            set(value) { props.disableGutters = value }
-        override var divider: Boolean
-            get() = props.divider
-            set(value) { props.divider = value }
-        override var focusVisibleClassName: ListItemAlignItem
-            get() = ListItemAlignItem.valueOf(props.focusVisibleClassName)
-            set(value) { props.focusVisibleClassName = value.value }
-        override var selected: Boolean
-            get() = props.selected
-            set(value) { props.selected = value }
-        override var style: String
-            get() = props.style
-            set(value) { props.style = value }
-    }
+    var Tag.alignItems: ListItemAlignItem
+        get() = ListItemAlignItem.value(@Suppress("UnsafeCastFromDynamic") props.asDynamic()["alignItems"])
+        set(value) { setProp("alignItems", value.value) }
+    var Tag.button: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["button"]
+        set(value) { setProp("button", value) }
+    var Tag.classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+    var Tag.containerComponent: String
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["containerComponent"]
+        set(value) { setProp("ContainerComponent", value) }
+    var Tag.containerProps: ListItemSecondaryActionProps
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["containerProps"]
+        set(value) { setProp("ContainerProps", value) }
+    var Tag.dense: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["dense"]
+        set(value) { setProp("dense", value) }
+    var Tag.disabled: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["disabled"]
+        set(value) { setProp("disabled", value) }
+    var Tag.disableGutters: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["disableGutters"]
+        set(value) { setProp("disableGutters", value) }
+    var Tag.divider: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["divider"]
+        set(value) { setProp("divider", value) }
+    var Tag.selected: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["selected"]
+        set(value) { setProp("selected", value) }
 }
