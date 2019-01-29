ccf2a92564d90a565d7ff4e5d7f4949d027f7167
subroh0508 <subroh.0508@gmail.com>
2019-01-29 22:08:21 +0900

    [fix]Fix MenuElementBuilder

--- core/src/main/kotlin/components/menu/MenuElementBuilder.kt
+++ core/src/main/kotlin/components/menu/MenuElementBuilder.kt
@@ -3,80 +3,76 @@ package components.menu
 import components.MaterialElementBuilder
 import components.menulist.MenuListProps
 import components.paper.PaperProps
-import kotlinext.js.jsObject
+import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
 import org.w3c.dom.events.Event
 import react.RComponent
+import react.RProps
 import react.RState
 import react.ReactElement
 import values.TransitionDuration
+import values.TransitionDuration.Auto.value
+import kotlin.reflect.KClass
 
-class MenuElementBuilder internal constructor(
-    override var type: RComponent<MenuProps, RState>,
-    override var attrs: MenuProps = jsObject {  }
-) : MaterialElementBuilder<MenuProps>(attrs),
-    MenuAttributes by AttributesImpl(attrs) {
+class MenuElementBuilder<T: Tag> internal constructor(
+    type: RComponent<RProps, RState>,
+    tag: KClass<T>,
+    factory: (TagConsumer<Unit>) -> T
+) : MaterialElementBuilder<T>(type, factory) {
 
-    internal class AttributesImpl(private val props: MenuProps) : MenuAttributes {
-        override var className: String?
-            get() = props.className
-            set(value) { props.className = value }
-        override var anchorEl: ReactElement?
-            get() = props.anchorEl
-            set(value) { props.anchorEl = value }
-        override var classes: Any
-            get() = props.classes
-            set(value) { props.classes = value }
-        override var disableAutoFocusItem: Boolean
-            get() = props.disableAutoFocusItem
-            set(value) { props.disableAutoFocusItem = value }
-        override var menuListProps: MenuListProps
-            get() = props.menuListProps
-            set(value) { props.menuListProps = value }
-        override var onClose: (Event) -> Unit
-            get() = props.onClose
-            set(value) { props.onClose = value }
-        override var onEnter: (Event) -> Unit
-            get() = props.onEnter
-            set(value) { props.onEnter = value }
-        override var onEntered: (Event) -> Unit
-            get() = props.onEntered
-            set(value) { props.onEntered = value }
-        override var onEntering: (Event) -> Unit
-            get() = props.onEntering
-            set(value) { props.onEntering = value }
-        override var onExit: (Event) -> Unit
-            get() = props.onExit
-            set(value) { props.onExit = value }
-        override var onExited: (Event) -> Unit
-            get() = props.onExited
-            set(value) { props.onExited = value }
-        override var onExiting: (Event) -> Unit
-            get() = props.onExiting
-            set(value) { props.onExiting = value }
-        override var open: Boolean
-            get() = props.open
-            set(value) { props.open = value }
-        override var paperProps: PaperProps
-            get() = props.paperProps
-            set(value) { props.paperProps = value }
-        override var popoverClasses: Any
-            get() = props.popoverClasses
-            set(value) { props.popoverClasses = value }
-        override var theme: Any
-            get() = props.theme
-            set(value) { props.theme = value }
-        override var transitionDuration: TransitionDuration
-            get() = when (props.transitionDuration) {
-                "auto" -> TransitionDuration.Auto
-                is Number -> TransitionDuration.Ms(props.transitionDuration as Double)
-                else -> TransitionDuration.EnterExit(
-                    enter = @Suppress("UnsafeCastFromDynamic") props.transitionDuration.asDynamic().enter,
-                    exit = @Suppress("UnsafeCastFromDynamic") props.transitionDuration.asDynamic().exit
-                )
-            }
-            set(value) { props.transitionDuration = value.value }
-        override var style: String
-            get() = props.style
-            set(value) { props.style = value }
-    }
+    var Tag.anchorEl: ReactElement?
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["anchorEl"]
+        set(value) { setProp("anchorEl", value) }
+    var Tag.classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+    var Tag.disableAutoFocusItem: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["disableAutoFocusItem"]
+        set(value) { setProp("disableAutoFocusItem", value) }
+    var Tag.menuListProps: MenuListProps
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["MenuListProps"]
+        set(value) { setProp("MenuListProps", value) }
+    var Tag.onClose: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onClose"]
+        set(value) { setProp("onClose", value) }
+    var Tag.onEnter: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEnter"]
+        set(value) { setProp("onEnter", value) }
+    var Tag.onEntered: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEntered"]
+        set(value) { setProp("onEntered", value) }
+    var Tag.onEntering: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEntering"]
+        set(value) { setProp("onEntering", value) }
+    var Tag.onExit: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExit"]
+        set(value) { setProp("onExit", value) }
+    var Tag.onExited: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExited"]
+        set(value) { setProp("onExited", value) }
+    var Tag.onExiting: (Event) -> Unit
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExiting"]
+        set(value) { setProp("onExiting", value) }
+    var Tag.open: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["open"]
+        set(value) { setProp("open", value) }
+    var Tag.paperProps: PaperProps
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["PaperProps"]
+        set(value) { setProp("PaperProps", value) }
+    var Tag.theme: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["theme"]
+        set(value) { setProp("theme", value) }
+    var Tag.popoverClasses: RProps
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["PopoverClasses"]
+        set(values) { setProp("PopoverClasses", value) }
+    var Tag.transitionDuration: TransitionDuration
+        get() = when (props.asDynamic()["transitionDuration"]) {
+            "auto" -> TransitionDuration.Auto
+            is Number -> TransitionDuration.Ms(props.asDynamic()["transitionDuration"] as Number)
+            else -> TransitionDuration.EnterExit(
+                enter = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["transitionDuration"].enter,
+                exit = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["transitionDuration"].exit
+            )
+        }
+        set(value) { setProp("transitionDuration", value.value) }
 }
