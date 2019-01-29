bd3947a31e4306d760a12722e25f1fb0a82536f9
subroh0508 <subroh.0508@gmail.com>
2019-01-29 21:07:27 +0900

    [edit]Fix IconElementBuilder

--- core/src/main/kotlin/components/icon/icon.kt
+++ core/src/main/kotlin/components/icon/icon.kt
@@ -1,14 +1,21 @@
 package components.icon
 
+import kotlinx.html.SPAN
+import kotlinx.html.Tag
 import react.RBuilder
 import react.RComponent
+import react.RProps
 import react.RState
+import kotlin.reflect.KClass
 
 @JsModule("@material-ui/core/Icon")
 private external val iconModule: dynamic
 
 @Suppress("UnsafeCastFromDynamic")
-private val iconComponent: RComponent<IconProps, RState> = iconModule.default
+private val iconComponent: RComponent<RProps, RState> = iconModule.default
 
-fun RBuilder.icon(iconName: String, block: IconElementBuilder.() -> Unit)
-    = child(IconElementBuilder(iconName, iconComponent).apply(block).create())
+fun RBuilder.icon(iconName: String, block: IconElementBuilder<SPAN>.() -> Unit)
+    = child(IconElementBuilder(iconName, iconComponent, SPAN::class) { SPAN(mapOf(), it) }.apply(block).create())
+
+fun <T: Tag> RBuilder.icon(tag: KClass<T>, iconName: String, block: IconElementBuilder<T>.() -> Unit)
+    = child(IconElementBuilder(iconName, iconComponent, tag).apply(block).create())
