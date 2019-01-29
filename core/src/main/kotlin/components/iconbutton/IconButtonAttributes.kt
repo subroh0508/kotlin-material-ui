1067b85d20c17153df30a4f0c82b96a86c842fe0
subroh0508 <subroh.0508@gmail.com>
2019-01-29 21:14:24 +0900

    [edit]Fix IconButtonElementBuilder

--- core/src/main/kotlin/components/iconbutton/IconButtonAttributes.kt
+++
@@ -1,7 +0,0 @@
-package components.iconbutton
-
-import components.button.enums.ButtonColor
-
-interface IconButtonAttributes {
-    var color: ButtonColor
-}
--- core/src/main/kotlin/components/iconbutton/iconButton.kt
+++ core/src/main/kotlin/components/iconbutton/iconButton.kt
@@ -1,14 +1,21 @@
 package components.iconbutton
 
+import kotlinx.html.BUTTON
+import kotlinx.html.Tag
 import react.RBuilder
 import react.RComponent
+import react.RProps
 import react.RState
+import kotlin.reflect.KClass
 
 @JsModule("@material-ui/core/IconButton")
 private external val iconButtonModule: dynamic
 
 @Suppress("UnsafeCastFromDynamic")
-private val iconButtonComponent: RComponent<IconButtonProps, RState> = iconButtonModule.default
+private val iconButtonComponent: RComponent<RProps, RState> = iconButtonModule.default
 
-fun RBuilder.iconButton(block: IconButtonElementBuilder.() -> Unit)
-    = child(IconButtonElementBuilder(iconButtonComponent).apply(block).create())
+fun RBuilder.iconButton(block: IconButtonElementBuilder<BUTTON>.() -> Unit)
+    = child(IconButtonElementBuilder(iconButtonComponent, BUTTON::class) { BUTTON(mapOf(), it) }.apply(block).create())
+
+fun <T: Tag> RBuilder.iconButton(tag: KClass<T>, block: IconButtonElementBuilder<T>.() -> Unit)
+    = child(IconButtonElementBuilder(iconButtonComponent, tag).apply(block).create())
