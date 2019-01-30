b6c0218850667f69cfc258ad01f2479a4d78bda8
subroh0508 <subroh.0508@gmail.com>
2019-01-30 21:48:29 +0900

    [fix]Fix typography extension method

--- core/src/main/kotlin/components/typography/typography.kt
+++ core/src/main/kotlin/components/typography/typography.kt
@@ -3,7 +3,6 @@ package components.typography
 import kotlinx.html.P
 import kotlinx.html.SPAN
 import kotlinx.html.Tag
-import kotlinx.html.attributesMapOf
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -17,12 +16,14 @@ private external val typographyModule: dynamic
 private val typographyComponent: RComponent<RProps, RState> = typographyModule.default
 
 fun RBuilder.typography(block: TypographyElementBuilder<SPAN>.() -> Unit)
-    = child(TypographyElementBuilder(typographyComponent, SPAN::class).apply(block).create())
-
-fun RBuilder.typography(block: TypographyElementBuilder<P>.() -> Unit)
-    = child(TypographyElementBuilder(typographyComponent, P::class) { P(attributesMapOf("paragraph", "true"), it) }.apply(block).create())
+    = child(TypographyElementBuilder(typographyComponent, SPAN::class) { SPAN(mapOf(), it) }.apply(block).create())
 
 fun <T: Tag> RBuilder.typography(tag: KClass<T>, block: TypographyElementBuilder<T>.() -> Unit)
-    = child(TypographyElementBuilder(typographyComponent, tag).apply(block).create())
+    = child(TypographyElementBuilder(typographyComponent, tag).apply {
+        block()
+        if (tag == P::class) {
+            setProp("paragraph", true)
+        }
+    }.create())
 
 
