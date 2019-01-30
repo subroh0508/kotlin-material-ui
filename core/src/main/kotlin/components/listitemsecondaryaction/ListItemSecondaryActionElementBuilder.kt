9dd050959722b7d3ffb7e7851bc0a740c11d83af
subroh0508 <subroh.0508@gmail.com>
2019-01-30 21:54:35 +0900

    [Add]Add ListItemSecondaryActionElementBuilder and required classes

---
+++ core/src/main/kotlin/components/listitemsecondaryaction/ListItemSecondaryActionElementBuilder.kt
@@ -0,0 +1,17 @@
+package components.listitemsecondaryaction
+
+import components.MaterialElementBuilder
+import kotlinx.html.DIV
+import kotlinx.html.Tag
+import react.RComponent
+import react.RProps
+import react.RState
+
+class ListItemSecondaryActionElementBuilder(
+    type: RComponent<RProps, RState>
+) : MaterialElementBuilder<DIV>(type, { DIV(mapOf(), it) }) {
+
+    var Tag.classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+}
--- core/src/main/kotlin/components/listitemsecondaryaction/ListItemSecondaryActionProps.kt
+++
@@ -1,6 +0,0 @@
-package components.listitemsecondaryaction
-
-import styled.StyledProps
-
-interface ListItemSecondaryActionProps : StyledProps {
-}
