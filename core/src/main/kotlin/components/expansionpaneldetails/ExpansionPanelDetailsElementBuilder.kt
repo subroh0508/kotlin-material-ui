184bd4553da3b55df2bbe029254b896d32851907
subroh0508 <subroh.0508@gmail.com>
2019-02-02 22:28:18 +0900

    [add]Add ExpansionPanelDetailsElementBuilder

---
+++ core/src/main/kotlin/components/expansionpaneldetails/ExpansionPanelDetailsElementBuilder.kt
@@ -0,0 +1,17 @@
+package components.expansionpaneldetails
+
+import components.MaterialElementBuilder
+import kotlinx.html.DIV
+import kotlinx.html.Tag
+import react.RComponent
+import react.RProps
+import react.RState
+
+class ExpansionPanelDetailsElementBuilder internal constructor(
+    type: RComponent<RProps, RState>
+) : MaterialElementBuilder<DIV>(type, { DIV(mapOf(), it) }) {
+
+    var Tag.classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+}
