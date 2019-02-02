7351b25c7088152abf987292c018f817afdce620
subroh0508 <subroh.0508@gmail.com>
2019-02-02 12:49:21 +0900

    [add]Add BottomNavigationElementBuilder

--- core/src/main/kotlin/components/badge/badge.kt
+++ core/src/main/kotlin/components/badge/badge.kt
@@ -1,10 +1,12 @@
 package components.badge
 
 import kotlinx.html.SPAN
+import kotlinx.html.Tag
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import kotlin.reflect.KClass
 
 @JsModule("@material-ui/core/Badge")
 private external val badgeModule: dynamic
@@ -14,3 +16,6 @@ private val badgeComponent: RComponent<RProps, RState> = badgeModule.default
 
 fun RBuilder.badge(block: BadgeElementBuilder<SPAN>.() -> Unit)
     = child(BadgeElementBuilder(badgeComponent, SPAN::class) { SPAN(mapOf(), it) }.apply(block).create())
+
+fun <T: Tag> RBuilder.badge(tag: KClass<T>, block: BadgeElementBuilder<T>.() -> Unit)
+    = child(BadgeElementBuilder(badgeComponent, tag).apply(block).create())
