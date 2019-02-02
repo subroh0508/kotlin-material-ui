2a0c36b282c221f130e355c3da9016a2bc5ace28
subroh0508 <subroh.0508@gmail.com>
2019-02-02 19:03:31 +0900

    [add]Add DialogContentElementBuilder

---
+++ core/src/main/kotlin/components/dialogcontent/DialogContentElementBuilder.kt
@@ -0,0 +1,16 @@
+package components.dialogcontent
+
+import components.MaterialElementBuilder
+import kotlinx.html.DIV
+import react.RComponent
+import react.RProps
+import react.RState
+
+class DialogContentElementBuilder internal constructor(
+    type: RComponent<RProps, RState>
+) : MaterialElementBuilder<DIV>(type, { DIV(mapOf(), it) }) {
+
+    var classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+}
