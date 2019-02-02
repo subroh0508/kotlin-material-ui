68fb755e196740480b146d0e9d81c3078c2bc8b4
subroh0508 <subroh.0508@gmail.com>
2019-02-02 23:14:00 +0900

    [add]Add RTransition interface and inherit Backdrop/Collapse that

--- core/src/main/kotlin/components/collapse/CollapseElementBuilder.kt
+++ core/src/main/kotlin/components/collapse/CollapseElementBuilder.kt
@@ -1,5 +1,6 @@
 package components.collapse
 
+import components.MaterialElementBuilder
 import components.consumers
 import kotlinx.css.LinearDimension
 import kotlinx.html.Tag
@@ -7,14 +8,19 @@ import kotlinx.html.TagConsumer
 import react.RComponent
 import react.RProps
 import react.RState
+import reacttransiton.RTransition
 import reacttransiton.RTransitionBuilder
 import kotlin.reflect.KClass
 
 class CollapseElementBuilder<T: Tag> internal constructor(
     type: RComponent<RProps, RState>,
     tag: KClass<T>,
-    factory: (TagConsumer<Unit>) -> T = consumers(tag)
-) : RTransitionBuilder<T>(type, factory) {
+    factory: (TagConsumer<Unit>) -> T = consumers(tag),
+    transition: RTransitionBuilder = RTransitionBuilder()
+) : MaterialElementBuilder<T>(type, factory), RTransition by transition {
+    init {
+        transition.props = props
+    }
 
     var Tag.classes: Any
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
