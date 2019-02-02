d923095b794e584723dbc76f16c7bca2c6ae6017
subroh0508 <subroh.0508@gmail.com>
2019-02-03 01:39:08 +0900

    [add]Add FormGroupElementBuilder

---
+++ core/src/main/kotlin/components/formgroup/formGroup.kt
@@ -0,0 +1,15 @@
+package components.formgroup
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+@JsModule("@material-ui/core/FormGroup")
+private external val formGroupModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val formGroupComponent: RComponent<RProps, RState> = formGroupModule.default
+
+fun RBuilder.formGroup(block: FormGroupElementBuilder.() -> Unit)
+    = child(FormGroupElementBuilder(formGroupComponent).apply(block).create())
