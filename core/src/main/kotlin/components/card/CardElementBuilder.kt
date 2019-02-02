4f6cd79c7727ed75023738907b0a51b08968f8db
subroh0508 <subroh.0508@gmail.com>
2019-02-02 13:33:17 +0900

    [add]Add CardElementBuilder

---
+++ core/src/main/kotlin/components/card/CardElementBuilder.kt
@@ -0,0 +1,21 @@
+package components.card
+
+import components.consumers
+import components.paper.PaperElementBuilder
+import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
+import react.RComponent
+import react.RProps
+import react.RState
+import kotlin.reflect.KClass
+
+class CardElementBuilder<T: Tag> internal constructor(
+    type: RComponent<RProps, RState>,
+    tag: KClass<T>,
+    factory: (TagConsumer<Unit>) -> T = consumers(tag)
+) : PaperElementBuilder<T>(type, tag, factory) {
+
+    var Tag.raised: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["raised"]
+        set(value) { setProp("raised", value) }
+}
