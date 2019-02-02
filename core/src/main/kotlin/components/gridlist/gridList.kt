e60e91a22689fc00d5e321fe2ec7d824971bcef0
subroh0508 <subroh.0508@gmail.com>
2019-02-03 03:08:27 +0900

    [add]Add GridListElementBuilder

---
+++ core/src/main/kotlin/components/gridlist/gridList.kt
@@ -0,0 +1,21 @@
+package components.gridlist
+
+import kotlinx.html.Tag
+import kotlinx.html.UL
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import kotlin.reflect.KClass
+
+@JsModule("@material-ui/core/GridList")
+private external val gridListModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val gridListComponent: RComponent<RProps, RState> = gridListModule.default
+
+fun RBuilder.gridList(block: GridListElementBuilder<UL>.() -> Unit)
+    = child(GridListElementBuilder(gridListComponent, UL::class) { UL(mapOf(), it) }.apply(block).create())
+
+fun <T: Tag> RBuilder.gridList(tag: KClass<T>, block: GridListElementBuilder<T>.() -> Unit)
+    = child(GridListElementBuilder(gridListComponent, tag).apply(block).create())
