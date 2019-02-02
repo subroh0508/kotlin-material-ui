a31ba2b389c402d27b00934c9f9124557c5b69d3
subroh0508 <subroh.0508@gmail.com>
2019-02-02 18:55:25 +0900

    [add]Add DialogElementBuilder

---
+++ core/src/main/kotlin/components/dialog/dialog.kt
@@ -0,0 +1,15 @@
+package components.dialog
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+@JsModule("@material-ui/core/Dialog")
+private external val dialogModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val dialogComponent: RComponent<RProps, RState> = dialogModule.default
+
+fun RBuilder.dialog(block: DialogElementBuilder.() -> Unit)
+    = child(DialogElementBuilder(dialogComponent).apply(block).create())
