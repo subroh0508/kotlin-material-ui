04a00b6f704e7e4c477fe644b7c653ad270447b1
subroh0508 <subroh.0508@gmail.com>
2019-02-03 00:54:49 +0900

    [add]Add FilledInputElementBuilder

---
+++ core/src/main/kotlin/components/filledinput/filledInput.kt
@@ -0,0 +1,15 @@
+package components.filledinput
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+@JsModule("@material-ui/core/FilledInput")
+private external val filledInputModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val filledInputComponent: RComponent<RProps, RState> = filledInputModule.default
+
+fun RBuilder.filledInput(block: FilledInputElementBuilder.() -> Unit)
+    = child(FilledInputElementBuilder(filledInputComponent).apply(block).create())
