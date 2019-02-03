8874ccb52028444d31fab4c13ae59f04825ee8db
subroh0508 <subroh.0508@gmail.com>
2019-02-03 22:40:23 +0900

    [add]Add SnackbarElementBuilder

---
+++ core/src/main/kotlin/components/snackbar/values/SnackbarTransitionDuration.kt
@@ -0,0 +1,12 @@
+package components.snackbar.values
+
+import kotlinext.js.js
+
+class SnackbarTransitionDuration {
+    constructor(ms: Number) { value = ms }
+    constructor(enter: Number, exit: Number) { value = js { this.enter = enter; this.exit = exit } as Any }
+
+    internal constructor(jsObject: dynamic) { value = jsObject as Any }
+
+    internal val value: Any
+}
