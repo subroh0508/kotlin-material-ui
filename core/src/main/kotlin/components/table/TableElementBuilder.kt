c8a48309eb5db45f13fc0083bdc8fdb2fddd54d9
subroh0508 <subroh.0508@gmail.com>
2019-02-03 23:50:57 +0900

    [add]Add TableElementBuilder

---
+++ core/src/main/kotlin/components/table/TableElementBuilder.kt
@@ -0,0 +1,25 @@
+package components.table
+
+import components.MaterialElementBuilder
+import components.consumers
+import components.table.enums.TablePadding
+import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
+import react.RComponent
+import react.RProps
+import react.RState
+import kotlin.reflect.KClass
+
+class TableElementBuilder<T: Tag> internal constructor(
+    type: RComponent<RProps, RState>,
+    tag: KClass<T>,
+    factory: (TagConsumer<Unit>) -> T = consumers(tag)
+) : MaterialElementBuilder<T>(type, factory) {
+
+    var Tag.classes: Any
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["classes"]
+        set(value) { setProp("classes", value) }
+    var Tag.padding: TablePadding
+        get() = TablePadding.valueOf(@Suppress("UnsafeCastFromDynamic") props.asDynamic()["padding"])
+        set(value) { setProp("padding", value.toString()) }
+}
