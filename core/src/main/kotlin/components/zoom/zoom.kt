5725db22e610cc45ff856e06e5470031b18abc63
subroh0508 <subroh.0508@gmail.com>
2019-02-04 01:53:21 +0900

    [add]Add ZoomElementBuilder

---
+++ core/src/main/kotlin/components/zoom/zoom.kt
@@ -0,0 +1,15 @@
+package components.zoom
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+@JsModule("@material-ui/core/Zoom")
+private external val zoomModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val zoomComponent: RComponent<RProps, RState> = zoomModule.default
+
+fun RBuilder.zoom(block: ZoomElementBuilder.() -> Unit)
+    = child(ZoomElementBuilder(zoomComponent).apply(block).create())
