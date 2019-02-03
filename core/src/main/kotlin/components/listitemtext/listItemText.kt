8e03f06ac8041e5c14216d46b9fbfbeaee13106b
subroh0508 <subroh.0508@gmail.com>
2019-02-03 19:27:32 +0900

    [add]Add ListItemTextElementBuilder

---
+++ core/src/main/kotlin/components/listitemtext/listItemText.kt
@@ -0,0 +1,15 @@
+package components.listitemtext
+
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+@JsModule("@material-ui/core/ListItemText")
+private external val listItemTextModule: dynamic
+
+@Suppress("UnsafeCastFromDynamic")
+private val listItemTextComponent: RComponent<RProps, RState> = listItemTextModule.default
+
+fun RBuilder.listItemText(block: ListItemTextElementBuilder.() -> Unit)
+    = child(ListItemTextElementBuilder(listItemTextComponent).apply(block).create())
