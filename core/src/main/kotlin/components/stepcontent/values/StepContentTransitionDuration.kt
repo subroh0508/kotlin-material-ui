85a0ea891052aa7a3ac8babcadc84ee2c7ed8f1f
subroh0508 <subroh.0508@gmail.com>
2019-02-03 23:10:15 +0900

    [add]Add StepContentElementBuilder

---
+++ core/src/main/kotlin/components/stepcontent/values/StepContentTransitionDuration.kt
@@ -0,0 +1,17 @@
+package components.stepcontent.values
+
+import kotlinext.js.js
+
+class StepContentTransitionDuration {
+    constructor(ms: Number) { value = ms }
+    constructor(enter: Number, exit: Number) { value = js { this.enter = enter; this.exit = exit } as Any }
+    private constructor() { value = "auto" }
+
+    companion object {
+        fun auto() = StepContentTransitionDuration()
+    }
+
+    internal constructor(jsObject: dynamic) { value = jsObject as Any }
+
+    internal val value: Any
+}
