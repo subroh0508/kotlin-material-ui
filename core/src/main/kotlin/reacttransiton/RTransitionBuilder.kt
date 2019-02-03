19c3e8183b0bdc63eec2a74b93fc56c12da7c53a
subroh0508 <subroh.0508@gmail.com>
2019-02-04 01:30:53 +0900

    [edit]Change extension method origin Tag to RProps

--- core/src/main/kotlin/reacttransiton/RTransitionBuilder.kt
+++ core/src/main/kotlin/reacttransiton/RTransitionBuilder.kt
@@ -1,6 +1,5 @@
 package reacttransiton
 
-import kotlinx.html.Tag
 import org.w3c.dom.events.Event
 import react.RProps
 import react.ReactElement
@@ -9,46 +8,46 @@ import reacttransiton.values.TransitionTimeout
 internal class RTransitionBuilder : RTransition {
     lateinit var props: RProps
 
-    override var Tag.`in`: Boolean
+    override var RProps.`in`: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["in"]
         set(value) { props.asDynamic()["in"] = value }
-    override var Tag.mountOnEnter: Boolean
+    override var RProps.mountOnEnter: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["mountOnEnter"]
         set(value) { props.asDynamic()["mountOnEnter"] = value }
-    override var Tag.unmountOnExit: Boolean
+    override var RProps.unmountOnExit: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["unmountOnExit"]
         set(value) { props.asDynamic()["unmountOnExit"] = value }
-    override var Tag.appear: Boolean
+    override var RProps.appear: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["appear"]
         set(value) { props.asDynamic()["appear"] = value }
-    override var Tag.enter: Boolean
+    override var RProps.enter: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["enter"]
         set(value) { props.asDynamic()["enter"] = value }
-    override var Tag.exit: Boolean
+    override var RProps.exit: Boolean
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["exit"]
         set(value) { props.asDynamic()["exit"] = value }
-    override var Tag.timeout: TransitionTimeout
+    override var RProps.timeout: TransitionTimeout
         get() = TransitionTimeout.fromDynamic(@Suppress("UnsafeCastFromDynamic") props.asDynamic()["timeout"])
         set(value) { props.asDynamic()["timeout"] = value.value }
-    override var Tag.addEndListener: (ReactElement, (Event) -> Unit) -> Unit
+    override var RProps.addEndListener: (ReactElement, (Event) -> Unit) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["addEndListener"]
         set(value) { props.asDynamic()["addEndListener"] = value }
-    override var Tag.onEnter: (ReactElement, Boolean) -> Unit
+    override var RProps.onEnter: (ReactElement, Boolean) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEnter"]
         set(value) { props.asDynamic()["onEnter"] = value }
-    override var Tag.onEntering: (ReactElement, Boolean) -> Unit
+    override var RProps.onEntering: (ReactElement, Boolean) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEntering"]
         set(value) { props.asDynamic()["onEntering"] = value }
-    override var Tag.onEntered: (ReactElement, Boolean) -> Unit
+    override var RProps.onEntered: (ReactElement, Boolean) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onEntered"]
         set(value) { props.asDynamic()["onEntered"] = value }
-    override var Tag.onExit: (ReactElement) -> Unit
+    override var RProps.onExit: (ReactElement) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExit"]
         set(value) { props.asDynamic()["onExit"] = value }
-    override var Tag.onExiting: (ReactElement) -> Unit
+    override var RProps.onExiting: (ReactElement) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExiting"]
         set(value) { props.asDynamic()["onExiting"] = value }
-    override var Tag.onExited: (ReactElement) -> Unit
+    override var RProps.onExited: (ReactElement) -> Unit
         get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["onExited"]
         set(value) { props.asDynamic()["onExited"] = value }
 }
