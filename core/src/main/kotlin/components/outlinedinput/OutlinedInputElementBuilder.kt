0f77f62b8f3982333ff826581e44c814410702f0
subroh0508 <subroh.0508@gmail.com>
2019-02-03 20:36:37 +0900

    [add]Add OutlinedInputElementBuilder

---
+++ core/src/main/kotlin/components/outlinedinput/OutlinedInputElementBuilder.kt
@@ -0,0 +1,19 @@
+package components.outlinedinput
+
+import components.inputbase.InputBaseElementBuilder
+import kotlinx.html.Tag
+import react.RComponent
+import react.RProps
+import react.RState
+
+class OutlinedInputElementBuilder internal constructor(
+    type: RComponent<RProps, RState>
+) : InputBaseElementBuilder(type) {
+
+    var Tag.labelWidth: Number
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["labelWidth"]
+        set(value) { setProp("labelWidth", value) }
+    var Tag.notched: Boolean
+        get() = @Suppress("UnsafeCastFromDynamic") props.asDynamic()["notched"]
+        set(value) { setProp("notched", value) }
+}
