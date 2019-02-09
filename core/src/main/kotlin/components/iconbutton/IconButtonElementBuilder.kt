9e5fff0f7793fc9ca6d9d2c9eaa99924f36a0f00
subroh0508 <subroh.0508@gmail.com>
2019-02-09 13:38:15 +0900

    [edit]Enable setting classes attributes to IconButton/Typography

--- core/src/main/kotlin/components/iconbutton/IconButtonElementBuilder.kt
+++ core/src/main/kotlin/components/iconbutton/IconButtonElementBuilder.kt
@@ -1,8 +1,11 @@
 package components.iconbutton
 
+import components.MaterialElementStyles
 import components.button.enums.ButtonColor
 import components.buttonbase.ButtonBaseElementBuilder
 import components.consumers
+import components.iconbutton.enums.IconButtonStyle
+import kotlinx.css.CSSBuilder
 import kotlinx.html.Tag
 import kotlinx.html.TagConsumer
 import react.RComponent
@@ -19,4 +22,8 @@ class IconButtonElementBuilder<T: Tag>(
     var Tag.color: ButtonColor
         get() = ButtonColor.valueOf(@Suppress("UnsafeCastFromDynamic") props.asDynamic()["color"])
         set(value) { setProp("color", value.toString()) }
+
+    fun MaterialElementStyles.styles(attrName: IconButtonStyle, handler: CSSBuilder.() -> Unit) {
+        this[attrName.toString()] = CSSBuilder().apply(handler).toDynamic
+    }
 }
