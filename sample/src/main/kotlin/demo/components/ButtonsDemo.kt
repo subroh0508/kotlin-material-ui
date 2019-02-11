e0c0e36a6500438fe2752c2ee4d3086cb1e24897
subroh0508 <subroh.0508@gmail.com>
2019-02-11 22:24:17 +0900

    [edit]Add link and upload button to ButtonsDemo

--- sample/src/main/kotlin/demo/components/ButtonsDemo.kt
+++ sample/src/main/kotlin/demo/components/ButtonsDemo.kt
@@ -4,20 +4,31 @@ import components.button.button
 import components.button.enums.ButtonColor
 import components.button.enums.ButtonVariant
 import kotlinx.css.*
+import kotlinx.html.InputType
+import kotlinx.html.SPAN
+import kotlinx.html.id
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import react.dom.div
-import react.dom.h2
+import react.dom.*
 import styled.css
 import styled.styledDiv
+import styles.childWithStyles
 
 class ButtonsDemo : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
+        val buttonStyle = props.asDynamic()["classes"]["button"] as String
+        val inputStyle = props.asDynamic()["classes"]["input"] as String
+
         div {
             h2 {
                 +"Contained Buttons"
+                a {
+                    attrs {
+                        href = "#contained-buttons"
+                    }
+                }
             }
         }
 
@@ -30,15 +41,10 @@ class ButtonsDemo : RComponent<RProps, RState>() {
             }
 
             styledDiv {
-                css {
-                    child("button") {
-                        margin(8.px)
-                    }
-                }
-
                 button {
                     attrs {
                         variant = ButtonVariant.contained
+                        rootClass(buttonStyle)
                     }
 
                     +"Default"
@@ -47,6 +53,7 @@ class ButtonsDemo : RComponent<RProps, RState>() {
                     attrs {
                         variant = ButtonVariant.contained
                         color = ButtonColor.primary
+                        rootClass(buttonStyle)
                     }
 
                     +"Primary"
@@ -55,6 +62,7 @@ class ButtonsDemo : RComponent<RProps, RState>() {
                     attrs {
                         variant = ButtonVariant.contained
                         color = ButtonColor.secondary
+                        rootClass(buttonStyle)
                     }
 
                     +"Secondary"
@@ -64,11 +72,57 @@ class ButtonsDemo : RComponent<RProps, RState>() {
                         variant = ButtonVariant.contained
                         color = ButtonColor.secondary
                         disabled = true
+                        rootClass(buttonStyle)
                     }
 
                     +"Disabled"
                 }
+                button {
+                    attrs {
+                        variant = ButtonVariant.contained
+                        href = "#contained-buttons"
+                        rootClass(buttonStyle)
+                    }
+
+                    +"Link"
+                }
+                input(classes = inputStyle) {
+                    attrs {
+                        accept = "image/*"
+                        id = "contained-button-file"
+                        multiple = true
+                        type = InputType.file
+                    }
+                }
+                label {
+                    setProp("htmlFor", "contained-button-file")
+
+                    button(SPAN::class, {
+                        attrs {
+                            variant = ButtonVariant.contained
+                            rootClass(buttonStyle)
+                        }
+
+                        +"Upload"
+                    }, { SPAN(mapOf(), it) })
+                }
             }
+        }
+    }
+
+    companion object {
+        fun render(rBuilder: RBuilder) = rBuilder.childWithStyles(
+            ButtonsDemo::class,
+            "button" to button,
+            "input" to input
+        ) { }
+
+        private val button = CSSBuilder().apply {
+            margin(8.px)
+        }
+
+        private val input = CSSBuilder().apply {
+            display = Display.none
         }
     }
 }
