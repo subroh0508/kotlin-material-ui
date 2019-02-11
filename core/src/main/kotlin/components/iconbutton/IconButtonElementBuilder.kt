b483e7c0ce6330f4cf2f84b89f7d4151526872c9
subroh0508 <subroh.0508@gmail.com>
2019-02-11 21:55:00 +0900

    [edit]Add classes method to IconButtonElementBuilder

--- core/src/main/kotlin/components/iconbutton/IconButtonElementBuilder.kt
+++ core/src/main/kotlin/components/iconbutton/IconButtonElementBuilder.kt
@@ -3,6 +3,7 @@ package components.iconbutton
 import components.button.enums.ButtonColor
 import components.buttonbase.ButtonBaseElementBuilder
 import components.consumers
+import components.iconbutton.enums.IconButtonStyle
 import kotlinx.html.Tag
 import kotlinx.html.TagConsumer
 import react.RComponent
@@ -16,6 +17,9 @@ class IconButtonElementBuilder<T: Tag>(
     factory: (TagConsumer<Unit>) -> T = consumers(tag)
 ) : ButtonBaseElementBuilder<T>(type, tag, factory) {
 
+    fun Tag.classes(vararg classMap: Pair<IconButtonStyle, String>) {
+        setClasses(classMap.map { it.first.toString() to it.second })
+    }
     var Tag.color: ButtonColor
         get() = ButtonColor.valueOf(@Suppress("UnsafeCastFromDynamic") props.asDynamic()["color"])
         set(value) { setProp("color", value.toString()) }
