73c9fe22ec31e274ea161f777b127ef8d1719382
subroh0508 <subroh.0508@gmail.com>
2019-02-11 21:03:07 +0900

    [edit]Add childWithStyles method to withStyles.kt

--- core/src/main/kotlin/styles/withStyles.kt
+++ core/src/main/kotlin/styles/withStyles.kt
@@ -1,9 +1,32 @@
 package styles
 
-import react.ReactElement
+import components.MaterialElementStyles
+import kotlinext.js.js
+import kotlinx.css.CSSBuilder
+import react.*
+import kotlin.reflect.KClass
 
 @JsModule("@material-ui/core/styles/withStyles")
 private external val withStylesModule: dynamic
 
 @Suppress("UnsafeCastFromDynamic")
-internal val withStyles: (dynamic) -> ((Any) -> ReactElement) = withStylesModule.default
+private val withStyles: (dynamic) -> ((Any) -> JsClass<*>) = withStylesModule.default
+
+private val MaterialElementStyles.toDynamic: Any
+    get() = js {
+        forEach { (key, value) ->
+            this[key] = value.toDynamic
+        }
+    } as Any
+
+private val CSSBuilder.toDynamic: Any
+    get() = js {
+        declarations.forEach { (key, value) ->
+            this[key] = value.toString()
+        }
+    } as Any
+
+fun <P : RProps, C : Component<P, *>> RBuilder.childWithStyles(klazz: KClass<C>, styles: MaterialElementStyles, handler: RHandler<P>): ReactElement {
+    val rClass = withStyles(styles.toDynamic)(klazz.js) as RClass<P>
+    return rClass(handler)
+}
