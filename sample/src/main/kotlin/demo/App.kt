9cacc1e2634ef5021d4f99cb6aa878b76cca7e83
subroh0508 <subroh.0508@gmail.com>
2019-02-17 00:24:13 +0900

    [edit]Change App/ButtonsDemo using MuiTheme

--- sample/src/main/kotlin/demo/App.kt
+++ sample/src/main/kotlin/demo/App.kt
@@ -3,6 +3,7 @@ package demo
 import demo.components.AppbarsDemo
 import demo.components.ButtonsDemo
 import demo.components.InputAdornmentsDemo
+import kotlinx.css.Color
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -11,28 +12,43 @@ import react.dom.code
 import react.dom.div
 import react.dom.h2
 import react.dom.p
+import styles.createMuiTheme
+import styles.muitheme.MuiTheme
+import styles.muitheme.primary
+import styles.muithemeprovider.muiThemeProvider
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        div {
-            h2 {
-                +"Welcome to React with Kotlin"
+        muiThemeProvider(theme) {
+            div {
+                h2 {
+                    +"Welcome to React with Kotlin"
+                }
+            }
+            p {
+                +"To get started, edit "
+                code { +"app/App.kt" }
+                +" and save to reload."
+            }
+            div {
+                AppbarsDemo.render(this)
+            }
+            div {
+                ButtonsDemo.render(this)
+            }
+            div {
+                InputAdornmentsDemo.render(this)
             }
         }
-        p {
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
-        }
+    }
 
-        div {
-            AppbarsDemo.render(this)
-        }
-        div {
-            ButtonsDemo.render(this)
-        }
-        div {
-            InputAdornmentsDemo.render(this)
+    companion object {
+        private val theme: MuiTheme = createMuiTheme {
+            palette {
+                primary {
+                    main = Color("#2196f3")
+                }
+            }
         }
     }
 }
