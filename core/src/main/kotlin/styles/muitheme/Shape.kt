e478a5889fd2481fa48911306bb7ca9feae1216b
subroh0508 <subroh.0508@gmail.com>
2019-02-16 16:06:55 +0900

    [edit]Add Shape/Spacing/ZIndex implementation

--- core/src/main/kotlin/styles/muitheme/Shape.kt
+++ core/src/main/kotlin/styles/muitheme/Shape.kt
@@ -1,3 +1,7 @@
 package styles.muitheme
 
 external interface Shape
+
+var Shape.borderRadius: Int
+    get() = asDynamic()["borderRadius"] as Int
+    set(value) { asDynamic()["borderRadius"] = value }
