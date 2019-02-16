33c566db24b3555afc24242305e68de28ea7fb3a
subroh0508 <subroh.0508@gmail.com>
2019-02-16 21:51:18 +0900

    [edit]Fix using enum methods to data class

--- core/src/main/kotlin/styles/muitheme/Mixin.kt
+++ core/src/main/kotlin/styles/muitheme/Mixin.kt
@@ -2,52 +2,55 @@
 
 package styles.muitheme
 
-import kotlinext.js.js
-import styles.muitheme.mixins.MixinGutter
-import styles.muitheme.mixins.MixinToolbar
+import kotlinext.js.jsObject
 
-external interface MixinGutterSets
-
-inline operator fun MixinGutterSets.get(key: MixinGutter): Int = asDynamic()[key.toString()] as Int
-inline operator fun MixinGutterSets.set(key: MixinGutter, value: Int) {
-    asDynamic()[key.toString()] = value
+data class MixinGutterSets internal constructor(
+    internal val jsObject: dynamic
+) {
+    var paddingLeft: Int
+        get() = jsObject["paddingLeft"] as Int
+        set(value) { jsObject["paddingLeft"] = value }
+    var paddingRight: Int
+        get() = jsObject["paddingRight"] as Int
+        set(value) { jsObject["paddingRight"] = value }
 }
 
-inline operator fun MixinGutterSets.get(key: String): MixinGutterSets = asDynamic()[key] as MixinGutterSets
-inline operator fun MixinGutterSets.set(key: String, handler: MixinGutterSets.() -> Unit) {
-    asDynamic()[key] = get(key).apply(handler)
+operator fun MixinGutterSets.get(key: String): MixinGutterSets = MixinGutterSets(jsObject[key])
+operator fun MixinGutterSets.set(key: String, handler: MixinGutterSets.() -> Unit) {
+    jsObject[key] = MixinGutterSets(jsObject { }).apply(handler)
 }
 
 fun mixinGutters(
     paddingLeft: Int, paddingRight: Int,
-    vararg obj: Pair<String, MixinGutterSets>
-) = js {
-    this["paddingLeft"] = paddingLeft
-    this["paddingRight"] = paddingRight
-    obj.forEach { o -> this[o.first] = o.second }
-} as MixinGutterSets
-
-external interface MixinToolbarSets
-
-inline operator fun MixinToolbarSets.get(key: MixinToolbar) = asDynamic()[key.toString()] as Int
-inline operator fun MixinToolbarSets.set(key: MixinToolbar, value: Int) {
-    asDynamic()[key.toString()] = value
+    vararg obj: Pair<String, MixinGutterSets.() -> Unit>
+) = MixinGutterSets(jsObject { }).also {
+    it.paddingLeft = paddingLeft
+    it.paddingRight = paddingRight
+    obj.forEach { o -> it[o.first] = o.second }
 }
 
-inline operator fun MixinToolbarSets.get(key: String): MixinToolbarSets = asDynamic()[key] as MixinToolbarSets
-inline operator fun MixinToolbarSets.set(key: String, handler: MixinToolbarSets.() -> Unit) {
-    asDynamic()[key] = get(key).apply(handler)
+data class MixinToolbarSets internal constructor(
+    internal val jsObject: dynamic
+) {
+    var minHeight: Int
+        get() = jsObject["minHeight"] as Int
+        set(value) { jsObject["minHeight"] = value }
+}
+
+operator fun MixinToolbarSets.get(key: String): MixinToolbarSets = MixinToolbarSets(jsObject[key])
+operator fun MixinToolbarSets.set(key: String, handler: MixinToolbarSets.() -> Unit) {
+    jsObject[key] = MixinToolbarSets(jsObject { }).apply(handler)
 }
 
 external interface Mixin
 
 var Mixin.gutters: (Any) -> MixinGutterSets
-    get() = asDynamic()["gutters"] as (Any) -> MixinGutterSets
-    set(value) { asDynamic()["gutters"] = value }
+    get() = { any: Any -> MixinGutterSets(asDynamic()["gutters"](any)) }
+    set(value) { asDynamic()["gutters"] = { any: Any -> value(any).jsObject } }
 
-inline fun Mixin.toolbar(handler: MixinToolbarSets.() -> Unit) {
-    asDynamic()["toolbar"] = toolbar.apply(handler)
+fun Mixin.toolbar(handler: MixinToolbarSets.() -> Unit) {
+    asDynamic()["toolbar"] = toolbar.apply(handler).jsObject
 }
 
 val Mixin.toolbar: MixinToolbarSets
-    get() = asDynamic()["toolbar"] as MixinToolbarSets
+    get() = MixinToolbarSets(asDynamic()["toolbar"])
