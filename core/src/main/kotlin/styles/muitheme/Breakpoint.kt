cef9e45b2d54da689953216493154020bfba5948
subroh0508 <subroh.0508@gmail.com>
2019-02-16 22:00:30 +0900

    [edit]Fix using BreakpointValue enums methods to data class

--- core/src/main/kotlin/styles/muitheme/Breakpoint.kt
+++ core/src/main/kotlin/styles/muitheme/Breakpoint.kt
@@ -2,23 +2,34 @@
 
 package styles.muitheme
 
-import styles.muitheme.breakpoints.BreakpointValue
-
-external interface BreakpointValueSets
-
-inline operator fun BreakpointValueSets.get(key: BreakpointValue): Int = asDynamic()[key.toString()] as Int
-inline operator fun BreakpointValueSets.set(key: BreakpointValue, value: Int) {
-    asDynamic()[key.toString()] = value
+data class BreakpointValueSets internal constructor(
+    internal val jsObject: dynamic
+) {
+    var xs: Int
+        get() = asDynamic()["xs"] as Int
+        set(value) { asDynamic()["xs"] = value }
+    var sm: Int
+        get() = asDynamic()["sm"] as Int
+        set(value) { asDynamic()["sm"] = value }
+    var md: Int
+        get() = asDynamic()["md"] as Int
+        set(value) { asDynamic()["md"] = value }
+    var lg: Int
+        get() = asDynamic()["lg"] as Int
+        set(value) { asDynamic()["lg"] = value }
+    var xl: Int
+        get() = asDynamic()["xl"] as Int
+        set(value) { asDynamic()["xl"] = value }
 }
 
 external interface Breakpoint
 
-inline fun Breakpoint.values(handler: BreakpointValueSets.() -> Unit) {
-    asDynamic()["values"] = values.apply(handler)
+fun Breakpoint.values(handler: BreakpointValueSets.() -> Unit) {
+    asDynamic()["values"] = values.apply(handler).jsObject
 }
 
 val Breakpoint.values: BreakpointValueSets
-    get() = asDynamic()["values"] as BreakpointValueSets
+    get() = BreakpointValueSets(asDynamic()["values"])
 
 var Breakpoint.up: (String) -> String
     get() = asDynamic()["up"] as (String) -> String
