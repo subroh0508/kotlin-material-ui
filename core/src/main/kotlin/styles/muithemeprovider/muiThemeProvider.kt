6978978fdb3be68fa23b9808c2f40646f400330e
subroh0508 <subroh.0508@gmail.com>
2019-02-16 23:10:50 +0900

    [fix]Pass theme instance to MuiThemeProviderBuilder

--- core/src/main/kotlin/styles/muithemeprovider/muiThemeProvider.kt
+++ core/src/main/kotlin/styles/muithemeprovider/muiThemeProvider.kt
@@ -4,6 +4,7 @@ import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import styles.muitheme.MuiTheme
 
 @JsModule("@material-ui/core/styles/MuiThemeProvider")
 private external val muiThemeProviderModule: dynamic
@@ -11,5 +12,5 @@ private external val muiThemeProviderModule: dynamic
 @Suppress("UnsafeCastFromDynamic")
 private val muiThemeProviderComponent: RComponent<RProps, RState> = muiThemeProviderModule.default
 
-fun RBuilder.muiThemeProvider(block: MuiThemeProviderBuilder.() -> Unit)
-    = child(MuiThemeProviderBuilder(muiThemeProviderComponent).apply(block).create())
+fun RBuilder.muiThemeProvider(theme: MuiTheme, block: MuiThemeProviderBuilder.() -> Unit)
+    = child(MuiThemeProviderBuilder(muiThemeProviderComponent, theme).apply(block).create())
