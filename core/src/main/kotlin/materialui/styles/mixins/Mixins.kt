497a03d72f82302d569754d693c690a473df2eab
subroh0508 <subroh.0508@gmail.com>
2019-05-01 01:40:21 +0900

    [add]First 令和 commit

--- core/src/main/kotlin/materialui/styles/mixins/Mixins.kt
+++ core/src/main/kotlin/materialui/styles/mixins/Mixins.kt
@@ -2,52 +2,11 @@
 
 package materialui.styles.mixins
 
-import kotlinext.js.Object
-import kotlinext.js.js
 import kotlinx.css.CSSBuilder
 
-external interface Mixins {
-    val gutters: (dynamic) -> dynamic
-    val toolbar: dynamic
-}
+external interface Mixins
 
-val Mixins.gutters: (CSSBuilder) -> CSSBuilder
-    get() {
-        val gutter = asDynamic()["gutters"] as (dynamic) -> (dynamic)
+val Mixins.gutters: Gutters by ReadOnlyGutterDelegate
+val Mixins.toolbar: CSSBuilder by ReadOnlyToolbarDelegate
 
-        return { cssBuilder: CSSBuilder ->
-            val jsObject = gutter.invoke(cssBuilder.toDynamic())
 
-            CSSBuilder().apply {
-                Object.keys(jsObject as Any).toList().forEach { key ->
-                    val value = jsObject[key]
-
-                    when (value) {
-                        is String, is Number -> declarations[key] = value
-                        else -> key()
-                    }
-                }
-            }
-        }
-    }
-
-private fun CSSBuilder.toDynamic(): dynamic = js {
-    declarations.forEach { (key, value) ->
-        if (value is CSSBuilder) {
-            this[key] = value.toDynamic()
-        } else {
-            this[key] = value
-        }
-    }
-}
-
-private fun buildCssBuilder(cssBuilder: CSSBuilder, jsObject: dynamic): CSSBuilder {
-    Object.keys(jsObject as Any).toList().forEach { key ->
-        val value = jsObject[key]
-
-        when (value) {
-            is String, is Number -> cssBuilder.declarations[key] = value
-            else -> key()
-        }
-    }
-}
