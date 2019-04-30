10420c32f394aaa46be9904577ec70d6e21a2f35
subroh0508 <subroh.0508@gmail.com>
2019-05-01 01:40:21 +0900

    [add]First 令和 commit

---
+++ core/src/main/kotlin/materialui/styles/mixins/MixinsDelegate.kt
@@ -0,0 +1,67 @@
+package materialui.styles.mixins
+
+import kotlinext.js.Object.keys
+import kotlinext.js.js
+import kotlinx.css.CSSBuilder
+import kotlin.reflect.KProperty
+
+typealias Gutters = (CSSBuilder) -> CSSBuilder
+
+object ReadOnlyGutterDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Gutters = { cssBuilder: CSSBuilder ->
+        val gutter = asDynamic()[property.name] as (dynamic) -> (dynamic)
+
+        buildCssBuilder(CSSBuilder(), gutter.invoke(cssBuilder.toDynamic()))
+    }
+}
+
+object GutterDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Gutters = { cssBuilder: CSSBuilder ->
+        val gutter = asDynamic()[property.name] as (dynamic) -> (dynamic)
+
+        buildCssBuilder(CSSBuilder(), gutter.invoke(cssBuilder.toDynamic()))
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Gutters?) {
+        asDynamic()[property.name] = { jsObject: dynamic ->
+            value?.invoke(buildCssBuilder(CSSBuilder(), jsObject))?.toDynamic()
+        }
+    }
+}
+
+object ReadOnlyToolbarDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): CSSBuilder
+        = buildCssBuilder(CSSBuilder(), asDynamic()[property.name])
+}
+
+object ToolbarDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): CSSBuilder
+        = buildCssBuilder(CSSBuilder(), asDynamic()[property.name])
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: CSSBuilder?) {
+        asDynamic()[property.name] = value?.toDynamic()
+    }
+}
+
+private fun CSSBuilder.toDynamic(): dynamic = js {
+    declarations.forEach { (key, value) ->
+        if (value is CSSBuilder) {
+            this[key] = value.toDynamic()
+        } else {
+            this[key] = value
+        }
+    }
+}
+
+
+
+private fun buildCssBuilder(cssBuilder: CSSBuilder, jsObject: dynamic): CSSBuilder
+    = cssBuilder.apply {
+        keys(jsObject as Any).toList().forEach { key: String ->
+            when (jsObject[key]) {
+                is String -> cssBuilder.declarations[key] = jsObject[key] as String
+                is Number -> cssBuilder.declarations[key] = jsObject[key] as Number
+                else -> key { buildCssBuilder(this, jsObject[key]) }
+            }
+        }
+    }
--- core/src/main/kotlin/materialui/styles/mixins/options/MixinsOptions.kt
+++ core/src/main/kotlin/materialui/styles/mixins/options/MixinsOptions.kt
@@ -1,6 +1,11 @@
 package materialui.styles.mixins.options
 
-external interface MixinsOptions {
-    var gutters: ((dynamic) -> dynamic)?
-    var toolbar: dynamic
-}
+import kotlinx.css.CSSBuilder
+import materialui.styles.mixins.GutterDelegate
+import materialui.styles.mixins.Gutters
+import materialui.styles.mixins.ToolbarDelegate
+
+external interface MixinsOptions
+
+var MixinsOptions.gutters: Gutters by GutterDelegate
+var MixinsOptions.toolbar: CSSBuilder by ToolbarDelegate
