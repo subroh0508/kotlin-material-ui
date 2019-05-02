7dbd56b7039bda20259d9105abf4151f561fd2db
subroh0508 <subroh.0508@gmail.com>
2019-05-03 02:07:56 +0900

    [delete]Delete unused classes

--- core/src/main/kotlin/materialui/styles/muitheme/Shape.kt
+++
@@ -1,7 +0,0 @@
-package materialui.styles.muitheme
-
-external interface Shape
-
-var Shape.borderRadius: Int
-    get() = asDynamic()["borderRadius"] as Int
-    set(value) { asDynamic()["borderRadius"] = value }
--- core/src/main/kotlin/materialui/styles/muitheme/Spacing.kt
+++
@@ -1,7 +0,0 @@
-package materialui.styles.muitheme
-
-external interface Spacing
-
-var Spacing.unit: Int
-    get() = asDynamic()["unit"] as Int
-    set(value) { asDynamic()["unit"] = value }
