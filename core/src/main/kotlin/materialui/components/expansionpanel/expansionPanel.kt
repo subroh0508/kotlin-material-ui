7622d8673f94434b48b26d47adf1458ad6946ccd
subroh0508 <subroh.0508@gmail.com>
2019-05-03 21:55:15 +0900

    [edit]Delete old PaperElementBuilder

--- core/src/main/kotlin/materialui/components/expansionpanel/expansionPanel.kt
+++ core/src/main/kotlin/materialui/components/expansionpanel/expansionPanel.kt
@@ -2,20 +2,31 @@ package materialui.components.expansionpanel
 
 import kotlinx.html.DIV
 import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
+import materialui.components.paper.PaperProps
+import materialui.components.paper.enums.PaperStyle
+import org.w3c.dom.events.Event
 import react.RBuilder
-import react.RComponent
+import react.RClass
 import react.RProps
-import react.RState
-import kotlin.reflect.KClass
 
 @JsModule("@material-ui/core/ExpansionPanel")
 private external val expansionPanelModule: dynamic
 
+external interface ExpansionPanelProps : PaperProps {
+    // todo replace TransitionComponent: dynamic/TransitionProps: RProps
+    var CollapseProps: RProps?
+    var defaultExpand: Boolean?
+    var disabled: Boolean?
+    var expanded: Boolean?
+    var onChange: ((Event, Boolean) -> Unit)?
+}
+
 @Suppress("UnsafeCastFromDynamic")
-private val expansionPanelComponent: RComponent<RProps, RState> = expansionPanelModule.default
+private val expansionPanelComponent: RClass<ExpansionPanelProps> = expansionPanelModule.default
 
-fun RBuilder.expansionPanel(block: ExpansionPanelElementBuilder<DIV>.() -> Unit)
-    = child(ExpansionPanelElementBuilder(expansionPanelComponent, DIV::class) { DIV(mapOf(), it) }.apply(block).create())
+fun RBuilder.expansionPanel(vararg classMap: Pair<PaperStyle, String>, block: ExpansionPanelElementBuilder<DIV>.() -> Unit)
+    = child(ExpansionPanelElementBuilder(expansionPanelComponent, classMap.toList()) { DIV(mapOf(), it) }.apply(block).create())
 
-fun <T: Tag> RBuilder.expansionPanel(tag: KClass<T>, block: ExpansionPanelElementBuilder<T>.() -> Unit)
-    = child(ExpansionPanelElementBuilder(expansionPanelComponent, tag).apply(block).create())
+fun <T: Tag> RBuilder.expansionPanel(vararg classMap: Pair<PaperStyle, String>, factory: (TagConsumer<Unit>) -> T, block: ExpansionPanelElementBuilder<T>.() -> Unit)
+    = child(ExpansionPanelElementBuilder(expansionPanelComponent, classMap.toList(), factory).apply(block).create())
--- core/src/main/kotlin/materialui/components/stepper/stepper.kt
+++ core/src/main/kotlin/materialui/components/stepper/stepper.kt
@@ -2,20 +2,29 @@ package materialui.components.stepper
 
 import kotlinx.html.DIV
 import kotlinx.html.Tag
+import kotlinx.html.TagConsumer
+import materialui.components.paper.PaperProps
+import materialui.components.paper.enums.PaperStyle
 import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
-import kotlin.reflect.KClass
+import react.RClass
+import react.ReactElement
 
 @JsModule("@material-ui/core/Stepper")
 private external val stepperModule: dynamic
 
+external interface StepperProps : PaperProps {
+    var activeStep: Number?
+    var alternativeLabel: Boolean?
+    var connector: ReactElement?
+    var nonLinear: Boolean?
+    var orientation: String?
+}
+
 @Suppress("UnsafeCastFromDynamic")
-private val stepperComponent: RComponent<RProps, RState> = stepperModule.default
+private val stepperComponent: RClass<StepperProps> = stepperModule.default
 
-fun RBuilder.stepper(block: StepperElementBuilder<DIV>.() -> Unit)
-    = child(StepperElementBuilder(stepperComponent, DIV::class) { DIV(mapOf(), it) }.apply(block).create())
+fun RBuilder.stepper(vararg classMap: Pair<PaperStyle, String>, block: StepperElementBuilder<DIV>.() -> Unit)
+    = child(StepperElementBuilder(stepperComponent, classMap.toList()) { DIV(mapOf(), it) }.apply(block).create())
 
-fun <T: Tag> RBuilder.stepper(tag: KClass<T>, block: StepperElementBuilder<T>.() -> Unit)
-    = child(StepperElementBuilder(stepperComponent, tag).apply(block).create())
+fun <T: Tag> RBuilder.stepper(vararg classMap: Pair<PaperStyle, String>, factory: (TagConsumer<Unit>) -> T, block: StepperElementBuilder<T>.() -> Unit)
+    = child(StepperElementBuilder(stepperComponent, classMap.toList(), factory).apply(block).create())
