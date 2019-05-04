2f51c4b4035b2fadc57c5f95b92452b903de649f
subroh0508 <subroh.0508@gmail.com>
2019-05-04 18:08:00 +0900

    [edit]Change RTransitionGroupBuilder having RClass

---
+++ core/src/main/kotlin/materialui/components/buttonbase/enums/TouchRippleStyle.kt
@@ -0,0 +1,8 @@
+package materialui.components.buttonbase.enums
+
+@Suppress("EnumEntryName")
+enum class TouchRippleStyle {
+    root,
+    ripple, rippleVisible, ripplePulsate,
+    child, childLeaving, childPulsate
+}
