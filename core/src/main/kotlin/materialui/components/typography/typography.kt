5ff1ddc9c7807262c702dc5fcf3317ab14526ab6
subroh0508 <subroh.0508@gmail.com>
2019-05-06 00:14:45 +0900

    [edit]Move HeadlineMapping to typography.kt

--- core/src/main/kotlin/materialui/components/typography/typography.kt
+++ core/src/main/kotlin/materialui/components/typography/typography.kt
@@ -26,6 +26,26 @@ external interface TypographyProps : StandardProps {
     var variant: String?
 }
 
+external interface HeadlineMapping {
+    var h1: String?
+    var h2: String?
+    var h3: String?
+    var h4: String?
+    var h5: String?
+    var h6: String?
+    var subtitle1: String?
+    var subtitle2: String?
+    var body1: String?
+    var body2: String?
+    var display4: String?
+    var display3: String?
+    var display2: String?
+    var display1: String?
+    var headline: String?
+    var title: String?
+    var subheading: String?
+}
+
 @Suppress("UnsafeCastFromDynamic")
 private val typographyComponent: RClass<TypographyProps> = typographyModule.default
 
