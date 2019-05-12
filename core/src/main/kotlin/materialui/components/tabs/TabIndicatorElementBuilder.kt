e00a267f9cbdb79d9eb64f173f242717744499e9
subroh0508 <subroh.0508@gmail.com>
2019-05-13 02:45:16 +0900

    [edit]Add css setter method

--- core/src/main/kotlin/materialui/components/tabs/TabIndicatorElementBuilder.kt
+++ core/src/main/kotlin/materialui/components/tabs/TabIndicatorElementBuilder.kt
@@ -1,12 +1,24 @@
 package materialui.components.tabs
 
+import kotlinx.css.CSSBuilder
+import kotlinx.css.RuleSet
 import kotlinx.html.SPAN
 import kotlinx.html.Tag
 import materialui.components.tabs.enums.TabsIndicatorColor
+import react.ReactElement
 import react.dom.RDOMBuilder
+import styled.Styled
 
 class TabIndicatorElementBuilder internal constructor() : RDOMBuilder<SPAN>({ SPAN(mapOf(), it) }) {
+    val css = CSSBuilder()
+
     var Tag.color: TabsIndicatorColor?
         get() = (asDynamic()["color"] as String?)?.let { TabsIndicatorColor.valueOf(it) }
         set(value) { asDynamic()["color"] = value?.toString() }
+
+    fun css(ruleSet: RuleSet) {
+        css.apply(ruleSet)
+    }
+
+    override fun create(): ReactElement = Styled.createElement(attrs.tagName, css, props, childList)
 }
