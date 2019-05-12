d8df8076567ac14b1132a16c4426fb5899e8f5e9
subroh0508 <subroh.0508@gmail.com>
2019-05-13 04:34:31 +0900

    v0.1.5

--- core/src/main/kotlin/materialui/components/tabs/TabIndicatorElementBuilder.kt
+++ core/src/main/kotlin/materialui/components/tabs/TabIndicatorElementBuilder.kt
@@ -5,20 +5,15 @@ import kotlinx.css.RuleSet
 import kotlinx.html.SPAN
 import kotlinx.html.Tag
 import materialui.components.tabs.enums.TabsIndicatorColor
-import react.ReactElement
+import materialui.styles.toDynamic
 import react.dom.RDOMBuilder
-import styled.Styled
 
 class TabIndicatorElementBuilder internal constructor() : RDOMBuilder<SPAN>({ SPAN(mapOf(), it) }) {
-    val css = CSSBuilder()
-
     var Tag.color: TabsIndicatorColor?
         get() = (asDynamic()["color"] as String?)?.let { TabsIndicatorColor.valueOf(it) }
         set(value) { asDynamic()["color"] = value?.toString() }
 
-    fun css(ruleSet: RuleSet) {
-        css.apply(ruleSet)
+    fun style(ruleSet: RuleSet) {
+        setProp("style", CSSBuilder().apply(ruleSet).toDynamic)
     }
-
-    override fun create(): ReactElement = Styled.createElement(attrs.tagName, css, props, childList)
 }
