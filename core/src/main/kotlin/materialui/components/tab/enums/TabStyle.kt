c64f01eec0c3731b202c5b8212ced2e2d50c8096
subroh0508 <subroh.0508@gmail.com>
2019-05-13 02:45:31 +0900

    [edit]Add TabStyle

---
+++ core/src/main/kotlin/materialui/components/tab/enums/TabStyle.kt
@@ -0,0 +1,17 @@
+package materialui.components.tab.enums
+
+@Suppress("EnumEntryName")
+enum class TabStyle {
+    root,
+    labelIcon,
+    textColorInherit,
+    textColorPrimary,
+    textColorSecondary,
+    selected,
+    disabled,
+    fullWidth,
+    wrapper,
+    labelContainer,
+    label,
+    labelWrapped
+}
