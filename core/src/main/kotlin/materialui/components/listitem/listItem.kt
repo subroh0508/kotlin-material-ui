c5956c9de1a405426ca661dde452c61960a4a9b0
subroh0508 <subroh.0508@gmail.com>
2019-05-30 20:24:48 +0900

    [edit]Add ButtonListItemElementBuilder

--- core/src/main/kotlin/materialui/components/listitem/listItem.kt
+++ core/src/main/kotlin/materialui/components/listitem/listItem.kt
@@ -1,10 +1,10 @@
 package materialui.components.listitem
 
-import kotlinx.html.DIV
 import kotlinx.html.LI
 import kotlinx.html.Tag
 import kotlinx.html.TagConsumer
 import materialui.components.StandardProps
+import materialui.components.buttonbase.ButtonBaseProps
 import materialui.components.listitem.enums.ListItemStyle
 import react.RBuilder
 import react.RClass
@@ -25,17 +25,16 @@ external interface ListItemProps : StandardProps {
     var selected: Boolean?
 }
 
+external interface ButtonListItemProps : ButtonBaseProps, ListItemProps
+
 @Suppress("UnsafeCastFromDynamic")
 private val listItemComponent: RClass<ListItemProps> = listItemModule.default
 
 fun RBuilder.listItem(vararg classMap: Pair<ListItemStyle, String>, block: ListItemElementBuilder<LI, ListItemProps>.() -> Unit)
     = child(ListItemElementBuilder(listItemComponent, classMap.toList()) { LI(mapOf(), it) }.apply(block).create())
 
-fun RBuilder.listItem(button: Boolean, vararg classMap: Pair<ListItemStyle, String>, block: ListItemElementBuilder<DIV, ListItemProps>.() -> Unit)
-    = child(ListItemElementBuilder(listItemComponent, classMap.toList()) { DIV(mapOf(), it) }.apply {
-        block()
-        attrs.button = button
-    }.create())
+fun RBuilder.buttonListItem(vararg classMap: Pair<ListItemStyle, String>, block: ButtonListItemElementBuilder.() -> Unit)
+    = child(ButtonListItemElementBuilder(listItemComponent, classMap.toList()).apply(block).create())
 
 fun <T: Tag> RBuilder.listItem(vararg classMap: Pair<ListItemStyle, String>, factory: (TagConsumer<Unit>) -> T, block: ListItemElementBuilder<T, ListItemProps>.() -> Unit)
     = child(ListItemElementBuilder(listItemComponent, classMap.toList(), factory).apply(block).create())
