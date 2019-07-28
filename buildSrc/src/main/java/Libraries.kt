364ca6ce5b9a2ead2380bc0ddababce845dec901
subroh0508 <subroh.0508@gmail.com>
2019-07-28 20:16:35 +0900

    [add]Add buildSrc module

---
+++ buildSrc/src/main/java/Libraries.kt
@@ -0,0 +1,17 @@
+@Suppress("unused")
+object Libraries {
+    object Plugin {
+        const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        const val kotlinFrontend = "org.jetbrains.kotlin:kotlin-frontend-plugin:${Versions.kotlinFrontendPlugin}"
+        const val kotlinxSerialization = "org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}"
+    }
+
+    object KotlinJs {
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-js:${Versions.kotlin}"
+        const val test = "org.jetbrains.kotlin:kotlin-test-js:${Versions.kotlin}"
+        const val html = "org.jetbrains.kotlinx:kotlinx-html-js:${Versions.kotlinxHtml}"
+        const val react = "org.jetbrains:kotlin-react:${Versions.kotlinReact}"
+        const val reactDom = "org.jetbrains:kotlin-react:${Versions.kotlinReact}"
+        const val styled = "org.jetbrains:kotlin-styled:${Versions.kotlinJsWrappers}"
+    }
+}
---
+++ buildSrc/src/main/java/Packages.kt
@@ -0,0 +1,4 @@
+object Packages {
+    const val group = "subroh0508.net.kotlinmaterialui"
+    const val version = "0.1.18"
+}
