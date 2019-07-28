9f47744732156761eade3964bfb38d2112248980
subroh0508 <subroh.0508@gmail.com>
2019-07-28 20:16:35 +0900

    [add]Add buildSrc module

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,18 @@
+@Suppress("unused")
+object Versions {
+    const val kotlin = "1.3.41"
+    const val kotlinFrontendPlugin = "0.0.45"
+    const val kotlinxHtml = "0.6.12"
+    const val kotlinReact = "16.8.6-pre.80-kotlin-1.3.41"
+    const val kotlinJsWrappers = "1.0.0-pre.80-kotlin-1.3.41"
+
+    const val bintray = "1.8.4"
+
+    object Npm {
+        const val coreJs = "~3.1.4"
+        const val materialUi = "3.9.2"
+        const val styledComponent = "3.4.10"
+        const val inlineStyledPrefixer = "~5.1.0"
+        const val react = "16.8.6"
+    }
+}
