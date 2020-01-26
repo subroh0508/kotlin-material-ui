f436d434c5aa213708d6fb8c9166d5d7cd108416
subroh0508 <subroh.0508@gmail.com>
2020-01-26 22:56:27 +0900

    [fix]Fix spacing function

--- core/src/main/kotlin/materialui/styles/muitheme/MuiTheme.kt
+++ core/src/main/kotlin/materialui/styles/muitheme/MuiTheme.kt
@@ -3,8 +3,8 @@
 package materialui.styles.muitheme
 
 import kotlinx.css.Direction
+import kotlinx.css.LinearDimension
 import kotlinx.css.properties.BoxShadows
-import kotlinx.css.px
 import materialui.styles.ReadOnlyDirectionDelegate
 import materialui.styles.breakpoint.Breakpoints
 import materialui.styles.mixins.Mixins
@@ -22,9 +22,12 @@ external interface MuiTheme {
     val typography: Typography
     val shape: Shape
     val transitions: Transitions
+    val spacing: dynamic
     val zIndex: ZIndex
 }
 
 val MuiTheme.direction: Direction by ReadOnlyDirectionDelegate
 val MuiTheme.shadows: List<BoxShadows> by ReadOnlyShadowsDelegate
-fun MuiTheme.spacing(unit: Int = 1) = unit.px
+fun MuiTheme.spacing(unit: Int = 1) = LinearDimension((spacing as (Int) -> String)(unit))
+fun MuiTheme.spacing(top: Int = 1, right: Int = 1, bottom: Int = 1, left: Int = 1) =
+    (spacing as (Int, Int, Int, Int) -> String)(top, right, bottom, left)
