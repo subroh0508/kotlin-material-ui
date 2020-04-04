83978893bcff555e14ad0ea757f3c1638a1e4a47
Bernhard Frauendienst <oxc@esotechnik.de>
2020-04-04 16:09:38 +0200

    [add]Add ButtonGroup

---
+++ core/src/main/kotlin/materialui/components/buttongroup/enums/ButtonGroupStyle.kt
@@ -0,0 +1,28 @@
+package materialui.components.buttongroup.enums
+
+@Suppress("EnumEntryName")
+enum class ButtonGroupStyle {
+    root,
+    contained,
+    disabled,
+    fullWidth,
+    vertical,
+    grouped,
+    groupedHorizontal,
+    groupedVertical,
+    groupedText,
+    groupedTextHorizontal,
+    groupedTextVertical,
+    groupedTextPrimary,
+    groupedTextSecondary,
+    groupedOutlined,
+    groupedOutlinedHorizontal,
+    groupedOutlinedVertical,
+    groupedOutlinedPrimary,
+    groupedOutlinedSecondary,
+    groupedContained,
+    groupedContainedHorizontal,
+    groupedContainedVertical,
+    groupedContainedPrimary,
+    groupedContainedSecondary
+}
