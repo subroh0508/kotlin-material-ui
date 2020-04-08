ecc09e857677d20e967005370643fd7b8ba95d24
subroh0508 <subroh.0508@gmail.com>
2020-04-08 21:25:41 +0900

    Core: v0.3.4 / Lab: v0.3.1

--- buildSrc/src/main/java/Packages.kt
+++ buildSrc/src/main/java/Packages.kt
@@ -1,5 +1,5 @@
 object Packages {
     const val group = "subroh0508.net.kotlinmaterialui"
-    const val version = "0.3.3"
-    const val labVersion = "0.3.0"
+    const val version = "0.3.4"
+    const val labVersion = "0.3.1"
 }
