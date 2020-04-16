aa5e9f0e988680d313ba93cc9e91e274ac3c4ee8
subroh0508 <subroh.0508@gmail.com>
2020-04-16 22:40:16 +0900

    [add]add makeStyles with props argument

--- core/src/main/kotlin/materialui/styles/StylesSet.kt
+++ core/src/main/kotlin/materialui/styles/StylesSet.kt
@@ -2,9 +2,11 @@ package materialui.styles
 
 import kotlinext.js.js
 import kotlinx.css.CSSBuilder
+import kotlinx.css.Overflow
 import kotlinx.css.RuleSet
 import kotlinx.css.hyphenize
 import materialui.styles.muitheme.MuiTheme
+import react.RProps
 
 interface StylesSet {
     val theme: MuiTheme
@@ -23,6 +25,25 @@ internal class StylesBuilder(
     fun toDynamic() = js {
         css.forEach { (key, value) -> this[key] = value.toDynamic }
     } as Any
+}
+
+interface CreateStylesSet<P: RProps> {
+    val theme: MuiTheme
+    val css: MutableMap<String, (P) -> dynamic>
+
+    operator fun String.invoke(block: CSSBuilder.(P) -> Unit): String {
+        css[this] = { props: P -> CSSBuilder().apply { block(props) }.toDynamic }
+        return this
+    }
+}
+
+internal class CreateStylesBuilder<P: RProps>(
+    override val theme: MuiTheme,
+    override val css: MutableMap<String, (P) -> dynamic> = mutableMapOf()
+): CreateStylesSet<P> {
+    fun toDynamic() = js {
+        css.forEach { (key, value) -> this[key] = value }
+    }
 }
 
 internal val CSSBuilder.toDynamic: Any
