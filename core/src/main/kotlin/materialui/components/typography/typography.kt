382f7a019394ec168cae63858dfd56d3aa2a1a31
Bernhard Frauendienst <oxc@esotechnik.de>
2020-04-20 00:28:53 +0200

    [add]Add convenience functions for typography h1-h6

--- core/src/main/kotlin/materialui/components/typography/typography.kt
+++ core/src/main/kotlin/materialui/components/typography/typography.kt
@@ -1,11 +1,9 @@
 package materialui.components.typography
 
-import kotlinx.html.P
-import kotlinx.html.SPAN
-import kotlinx.html.Tag
-import kotlinx.html.TagConsumer
+import kotlinx.html.*
 import materialui.components.StandardProps
 import materialui.components.typography.enums.TypographyStyle
+import materialui.components.typography.enums.TypographyVariant
 import materialui.styles.muitheme.MuiTheme
 import react.RBuilder
 import react.RClass
@@ -51,3 +49,34 @@ fun RBuilder.typography(vararg classMap: Pair<TypographyStyle, String>, p: Boole
 
 fun <T: Tag> RBuilder.typography(vararg classMap: Pair<TypographyStyle, String>, factory: (TagConsumer<Unit>) -> T, block: TypographyElementBuilder<T, TypographyProps>.() -> Unit)
      = child(TypographyElementBuilder(typographyComponent, classMap.toList(), factory).apply(block).create())
+
+inline fun RBuilder.typographyH1(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H1, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H1(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h1
+        block()
+    }
+inline fun RBuilder.typographyH2(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H2, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H2(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h2
+        block()
+    }
+inline fun RBuilder.typographyH3(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H3, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H3(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h3
+        block()
+    }
+inline fun RBuilder.typographyH4(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H4, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H4(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h4
+        block()
+    }
+inline fun RBuilder.typographyH5(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H5, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H5(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h5
+        block()
+    }
+inline fun RBuilder.typographyH6(vararg classMap: Pair<TypographyStyle, String>, crossinline block: TypographyElementBuilder<H6, TypographyProps>.() -> Unit)
+    = typography(*classMap, factory = { H6(mapOf(), it) }) {
+        attrs.variant = TypographyVariant.h6
+        block()
+    }
