38c576e782222a3b0e894847bce74082b8d86720
subroh0508 <subroh.0508@gmail.com>
2020-04-28 00:03:38 +0900

    Revert "[edit]Enable classes property on CssBaseline"
    This reverts commit 603c88b0

--- core/src/main/kotlin/materialui/components/cssbaseline/cssBaseline.kt
+++ core/src/main/kotlin/materialui/components/cssbaseline/cssBaseline.kt
@@ -1,21 +1,18 @@
 package materialui.components.cssbaseline
 
 import kotlinext.js.jsObject
-import react.*
+import react.RBuilder
+import react.RClass
+import react.RProps
+import react.createElement
 
 @JsModule("@material-ui/core/CssBaseline")
 private external val cssBaselineModule: dynamic
 
-external interface CssBaselineProps : RProps {
-    var classes: dynamic
-}
-
-fun CssBaselineProps.classes(vararg className: String) {
-    classes = className.joinToString(" ")
-}
+external interface CssBaselineProps : RProps
 
 @Suppress("UnsafeCastFromDynamic")
 private val cssBaselineComponent: RClass<CssBaselineProps> = cssBaselineModule.default
 
-fun RBuilder.cssBaseline(props: CssBaselineProps = jsObject(), block: RHandler<CssBaselineProps>) =
-    child(cssBaselineComponent, props, block)
+fun RBuilder.cssBaseline(block: RBuilder.() -> Unit)
+    = child(createElement(cssBaselineComponent, jsObject<CssBaselineProps> { }, *RBuilder().apply(block).childList.toTypedArray()))
