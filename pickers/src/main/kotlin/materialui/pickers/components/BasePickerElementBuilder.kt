7631c30febb18de3e768d9e940da47b5f3c879fd
subroh0508 <subroh.0508@gmail.com>
2020-05-05 02:09:23 +0900

    [add]Add Tag extension properties to BasePickerElementBuilder

--- pickers/src/main/kotlin/materialui/pickers/components/BasePickerElementBuilder.kt
+++ pickers/src/main/kotlin/materialui/pickers/components/BasePickerElementBuilder.kt
@@ -4,10 +4,10 @@ import kotlinext.js.Object
 import kotlinext.js.jsObject
 import kotlinx.html.Tag
 import kotlinx.html.TagConsumer
-import react.RClass
-import react.ReactElement
-import react.createElement
+import react.*
 import react.dom.RDOMBuilder
+import kotlin.js.Date
+import kotlin.reflect.KClass
 
 abstract class BasePickerElementBuilder<T: Tag, Props: BasePickerProps> internal constructor(
     private val type: RClass<Props>,
@@ -25,4 +25,46 @@ abstract class BasePickerElementBuilder<T: Tag, Props: BasePickerProps> internal
 
         return createElement(type, props, *childList.toTypedArray())
     }
+
+    var Tag.value: Any? by pickerProps
+    fun Tag.value(v: String?) { value = v }
+    fun Tag.value(v: Number?) { value = v }
+    fun Tag.value(v: Date?) { value = v }
+
+    var Tag.onChange: ((dynamic, String?) -> Unit)? by pickerProps
+    fun Tag.onChange(block: (Any, String?) -> Unit) { onChange = block }
+
+    var Tag.autoOk: Boolean? by pickerProps
+    var Tag.inputFormat: String? by pickerProps
+    var Tag.disabled: Boolean? by pickerProps
+    var Tag.readOnly: Boolean? by pickerProps
+
+    var Tag.defaultHighlight: Any? by pickerProps
+
+    var Tag.onAccept: ((dynamic) -> Unit)? by pickerProps
+    fun Tag.onAccept(block: (Any) -> Unit) { onAccept = block }
+
+    var Tag.onError: ((dynamic, dynamic) -> Unit)? by pickerProps
+    fun Tag.onError(block: (Any, Any) -> Unit) { onError = block }
+
+    var Tag.onOpen: (() -> Unit)? by pickerProps
+    fun Tag.onOpen(block: () -> Unit) { onOpen = block }
+
+    var Tag.onClose: (() -> Unit)? by pickerProps
+    fun Tag.onClose(block: () -> Unit) { onClose = block }
+
+    var Tag.open: Boolean? by pickerProps
+    var Tag.showToolbar: Boolean? by pickerProps
+    var Tag.orientation: PickerOrientation? by pickerProps
+    @Suppress("PropertyName")
+    var Tag.ToolbarComponent: Any? by pickerProps
+    @Suppress("FunctionName")
+    fun <P: ToolbarComponentProps, C: Component<P, *>> Tag.ToolbarComponent(kClass: KClass<C>) { ToolbarComponent = kClass.rClass }
+    @Suppress("FunctionName")
+    fun <P: ToolbarComponentProps> Tag.ToolbarComponent(functionalComponent: FunctionalComponent<P>) { ToolbarComponent = functionalComponent }
+
+    var Tag.toolbarTitle: Any? by pickerProps
+    fun Tag.toolbarTitle(block: RBuilder.() -> Unit) { toolbarTitle = buildElement(block) }
+
+    var Tag.toolbarFormat: String? by pickerProps
 }
--- pickers/src/main/kotlin/materialui/pickers/components/BasePickerProps.kt
+++ pickers/src/main/kotlin/materialui/pickers/components/BasePickerProps.kt
@@ -1,5 +1,8 @@
 package materialui.pickers.components
 
+import org.w3c.dom.events.Event
+import react.RProps
+import react.RRef
 import react.dom.WithClassName
 import kotlin.reflect.KProperty
 
@@ -21,6 +24,79 @@ external interface BasePickerProps : WithClassName {
     var ToolbarComponent: dynamic
     var toolbarTitle: dynamic
     var toolbarFormat: String?
+}
+
+@Suppress("EnumEntryName")
+enum class PickerOrientation {
+    portrait, landscape
+}
+
+external interface DayProps : RProps {
+    var day: dynamic
+    var focused: Boolean?
+    var focusable: Boolean?
+    var inCurrentMonth: Boolean?
+    var isAnimating: Boolean?
+    var today: Boolean?
+    var disabled: Boolean?
+    var selected: Boolean?
+    var allowKeyboardControl: Boolean?
+    var disableMargin: Boolean?
+    var showDaysOutsideCurrentMonth: Boolean?
+    var disableHighlightToday: Boolean?
+    var onDayFocus: ((dynamic) -> Unit)?
+    var onDaySelect: ((dynamic, dynamic) -> Unit)?
+
+    /* ButtonBaseProps properties */
+    var buttonRef: RRef?
+    var centerRipple: Boolean?
+    var disableRipple: Boolean?
+    var disableTouchRipple: Boolean?
+    var focusRipple: Boolean?
+    var focusVisibleClassName: String?
+    var onFocusVisible: ((Event) -> Unit)?
+    var touchRippleProps: RProps?
+    var type: String?
+}
+
+external interface ExportedCalendarProps : RProps {
+    var showDaysOutsideCurrentMonth: Boolean?
+    var disableHighlightToday: Boolean?
+    //var onChange: ((dynamic, dynamic) -> Unit)?  TODO delete
+    var disablePast: Boolean?
+    var disableFuture: Boolean?
+    var renderDay: ((dynamic, dynamic, DayProps) -> dynamic)?
+    var allowKeyboardController: Boolean?
+    var loadingIndicator: dynamic
+}
+
+external interface ExportedClockViewProps : RProps {
+    var ampm: Boolean?
+    var minutesStep: Int?
+    var ampmInClock: Boolean?
+    var allowKeyboardControl: Boolean?
+    var minTime: dynamic
+    var maxTime: dynamic
+    var shouldDisableTime: ((Int, String) -> Boolean)?
+    var disableTimeValidationIgnoreDatePart: Boolean?
+}
+
+external interface ToolbarComponentProps : ExportedCalendarProps, ExportedClockViewProps {
+    var views: Array<String>?
+    var openView: String?
+    var date: dynamic
+    var setOpenView: ((String) -> Unit)
+    var onChange: ((dynamic, Boolean) -> Unit)?
+    var toolbarTitle: dynamic
+    var toolbarFormat: String?
+    var hideTabs: Boolean?
+    var dateRangeIcon: dynamic
+    var timeIcon: dynamic
+    var isLandscape: Boolean?
+    //var ampmInClock: Boolean? TODO delete
+    var isMobileKeyBoardViewOpen: Boolean?
+    var toggleMobileKeyboardView: Boolean?
+    var getMobileKeyboardInputViewButtonText: (() -> String)?
 }
 
 operator fun BasePickerProps.get(key: String): Any? = asDynamic()[key]
