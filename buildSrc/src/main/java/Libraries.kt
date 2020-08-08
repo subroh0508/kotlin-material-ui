f30eb2c34ce47289d96be17bc1a91984f3895710
subroh0508 <subroh.0508@gmail.com>
2020-08-08 22:00:30 +0900

    [update]Update kotlin-wrappers -> pre.110

--- buildSrc/src/main/java/Libraries.kt
+++ buildSrc/src/main/java/Libraries.kt
@@ -8,7 +8,7 @@ object Libraries {
 
     object Kotlin {
         const val version = "1.3.72"
-        const val wrappersBuild = "pre.109-kotlin-$version"
+        const val wrappersBuild = "pre.110-kotlin-$version"
 
         const val js = "org.jetbrains.kotlin:kotlin-stdlib-js:$version"
         const val jsTest = "org.jetbrains.kotlin:kotlin-test-js:$version"
