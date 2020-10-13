ad6f0a08560ccb01ddf36cf39f3228431a739ebc
subroh0508 <subroh.0508@gmail.com>
2020-10-14 00:06:01 +0900

    v0.5.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.4.0"))
+    implementation(kotlin("gradle-plugin", "1.4.10"))
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
 }
