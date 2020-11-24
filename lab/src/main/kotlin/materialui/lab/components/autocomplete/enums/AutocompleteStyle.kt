0953b5e3d1379393e18d5def62082db04ba716cd
Hamed Soltani <hmd.sb.1700@gmail.com>
2020-11-24 17:02:11 +0330

    Add autocomplete component

---
+++ lab/src/main/kotlin/materialui/lab/components/autocomplete/enums/AutocompleteStyle.kt
@@ -0,0 +1,26 @@
+package materialui.lab.components.autocomplete.enums
+
+@Suppress("EnumEntryName")
+enum class AutocompleteStyle {
+     root,
+     focused,
+     tag,
+     tagSizeSmall,
+     inputRoot,
+     input,
+     inputFocused,
+     endAdornment,
+     clearIndicator,
+     clearIndicatorDirty,
+     popupIndicator,
+     popupIndicatorOpen,
+     popper,
+     popperDisablePortal,
+     paper,
+     listbox,
+     loading,
+     noOptions,
+     option,
+     groupLabel,
+     groupUl,
+}
