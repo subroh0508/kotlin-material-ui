f358b5dd65ca542b615c937ebfb4e2b3c9b29c5a
subroh0508 <subroh.0508@gmail.com>
2021-02-22 02:03:19 +0900

    [update]Update Kotlin -> 1.4.30

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.4.21"))
+    implementation(kotlin("gradle-plugin", "1.4.30"))
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
 }
