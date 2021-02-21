112abfc6cd5b8e1ca8dce4fa9d1935434544a21d
subroh0508 <subroh.0508@gmail.com>
2021-02-22 02:44:45 +0900

    v0.5.5

--- buildSrc/src/main/java/maven-publishing.gradle.kts
+++ buildSrc/src/main/java/maven-publishing.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 val group = "net.subroh0508.kotlinmaterialui"
-val libVersion = "0.5.4"
+val libVersion = "0.5.5"
 
 val siteUrl = "https://github.com/subroh0508/kotlin-material-ui"
 val githubUrl = "https://github.com/subroh0508/kotlin-material-ui"
