ed8ffc846a188c9491783c7d6aa241995d12d074
subroh0508 <subroh.0508@gmail.com>
2021-04-16 00:34:33 +0900

    [edit]Enable publish to MavenCentral

--- buildSrc/src/main/java/maven-publishing.gradle.kts
+++ buildSrc/src/main/java/maven-publishing.gradle.kts
@@ -1,9 +1,8 @@
-import groovy.util.Node
 import java.text.SimpleDateFormat
 
 plugins {
-    `maven-publish`
     signing
+    `maven-publish`
 }
 
 val group = "net.subroh0508.kotlinmaterialui"
@@ -14,40 +13,71 @@ val githubUrl = "https://github.com/subroh0508/kotlin-material-ui"
 
 val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
 
-publishing.publications {
-    create<MavenPublication>("ToMavenPublication") {
-        from(components["kotlin"])
-        artifact(tasks.getByName<Zip>("jsLegacySourcesJar"))
-        groupId = group
-        artifactId = project.name
-        version = libVersion
-        pom.withXml {
-            asNode().also { root ->
-                root.appendNode("description", "Kotlin Wrapper Library of Material-UI")
-                root.appendNode("name", "Kotlin-Material-UI${if (project.name == "core") "" else "/Lab"}")
-                root.appendNode("url", githubUrl)
-                root.appendPomConfig()
+publishing {
+    publications {
+        create<MavenPublication>("kotlin") {
+            from(components["kotlin"])
+            artifact(tasks.getByName<Zip>("jsLegacySourcesJar"))
+            groupId = group
+            artifactId = project.name
+            version = libVersion
+
+            metadata()
+        }
+    }
+    val repoNameSuffix = when (project.name) {
+        "lab" -> "-Lab"
+        "pickers" -> "-Pickers"
+        "date-io" -> "-DateIO"
+        "date-fns" -> "-DateIO-date-fns"
+        else -> ""
+    }
+
+    val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+
+    val user = project.property("sonatypeUsername")?.toString()
+    val key = project.property("sonatypePassword")?.toString()
+
+    repositories {
+        maven {
+            name = "Kotlin-Material-UI$repoNameSuffix"
+            url = uri(releasesRepoUrl)
+            credentials {
+                username = user
+                password = key
             }
         }
     }
 }
 
-fun Node.appendPomConfig() {
-    Node(this, "licenses").also { licenses ->
-        Node(licenses, "license").also { license ->
-            license.appendNode("name", "The MIT Licenses")
-            license.appendNode("url", "https://opensource.org/licenses/MIT")
-            license.appendNode("distribution", "repo")
+signing {
+    sign(publishing.publications["kotlin"])
+}
+
+fun MavenPublication.metadata() {
+    pom {
+        name.set("Kotlin-Material-UI${if (project.name == "core") "" else "/Lab"}")
+        description.set("Kotlin Wrapper Library of Material-UI")
+        url.set(githubUrl)
+
+        licenses {
+            license {
+                name.set("The MIT Licenses")
+                url.set("https://opensource.org/licenses/MIT")
+            }
         }
-    }
-    Node(this, "developers").also { developers ->
-        Node(developers, "developer").also { developer ->
-            developer.appendNode("id", "subroh0508")
-            developer.appendNode("name", "Subroh Nishikori")
-            developer.appendNode("email", "in-the-n@me-of.love")
+
+        developers {
+            developer {
+                id.set("subroh0508")
+                name.set("Subroh Nishikori")
+                email.set("in-the-n@me-of.love")
+            }
         }
-    }
-    Node(this, "scm").also { scm ->
-        scm.appendNode("url", githubUrl)
+
+        scm {
+            url.set(githubUrl)
+        }
     }
 }
