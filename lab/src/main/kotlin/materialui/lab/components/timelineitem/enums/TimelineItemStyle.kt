f9e3613996d399ebbb3d78d5a6e8ce37311565c9
hamed-sb <hmd.sb.1700@gmail.com>
2021-06-23 10:38:35 +0430

    [add]Add timeline components

---
+++ lab/src/main/kotlin/materialui/lab/components/timelineitem/enums/TimelineItemStyle.kt
@@ -0,0 +1,10 @@
+package materialui.lab.components.timelineitem.enums
+
+@Suppress("EnumEntryName")
+enum class TimelineItemStyle {
+    root,
+    alignLeft,
+    alignRight,
+    alignAlternate,
+    missingOppositeContent,
+}
